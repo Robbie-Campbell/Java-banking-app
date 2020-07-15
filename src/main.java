b4ad40ed26b4dab3922788ffa6fef221a9a5a3c6
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-07-15 11:16:54 +0100

    It works!

--- src/main.java
+++ src/main.java
@@ -1,25 +1,32 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-class Banking{
+abstract class Banking implements ActionListener {
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
     private static void bank_app() {
         try {
             File check_balance = new File("balance.txt");
             Scanner checker = new Scanner(check_balance);
-            int current_balance = checker.nextInt();
+            final float[] current_balance = {checker.nextFloat()};
 
             JLabel title = new JLabel("Welcome to the transaction app");
             JLabel input_prompt = new JLabel("Input amount: ");
-            JTextField input_amount = new JTextField(20);
+            JLabel select = new JLabel("Select transaction type: ");
+            JTextField input_amount = new JTextField(10);
             JComboBox<String> type_of_transaction = new JComboBox<>();
             JButton enter_amount = new JButton("Enter");
-            JLabel current_balance_display = new JLabel(String.format("Current balance is: %d", current_balance));
+            JButton another_transaction = new JButton("Perform another transaction?");
+            JLabel current_balance_display = new JLabel(String.format("Current balance is: £%s", df2.format(current_balance[0])));
 
             /////////// GUI elements set up ///////////////////
 
+            type_of_transaction.addItem("");
             type_of_transaction.addItem("Withdrawal");
             type_of_transaction.addItem("Deposit");
 
@@ -59,82 +66,84 @@ class Banking{
 
             constraints.gridy = 2;
             constraints.gridx = 0;
+            main_panel.add(select, constraints);
+
+            constraints.gridy = 3;
+            constraints.gridx = 0;
             main_panel.add(type_of_transaction, constraints);
 
             constraints.gridx = 1;
             main_panel.add(enter_amount, constraints);
-        }catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-        boolean running = true;
-        while (running) {
-            try {
-                File check_balance = new File("balance.txt");
-                Scanner checker = new Scanner(check_balance);
-                int current_balance = checker.nextInt();
-                String transaction_type;
-                String past_tense_type;
-                System.out.println("What kind of transaction do you need to perform?: ");
-                Scanner input_type = new Scanner(System.in);
-                String i = input_type.nextLine();
-                switch (i) {
 
-                    case "deposit":
-                        System.out.println("Deposit selected.");
-                        transaction_type = "deposit";
-                        past_tense_type = "deposited";
-                        break;
-                    case "withdrawal":
-                        System.out.println("Withdrawal selected");
-                        transaction_type = "withdraw";
-                        past_tense_type = "withdrawn";
-                        break;
-                    case "cancel":
-                        System.out.println("Thank you");
-                        running = false;
-                        return;
-                    default:
-                        System.out.println("Transaction cancelled.");
-                        continue;
-                }
+            constraints.gridy = 4;
+            constraints.gridx = 0;
+            another_transaction.setVisible(false);
+            main_panel.add(another_transaction, constraints);
 
-                System.out.println("Your current balance is £" + current_balance);
-                System.out.printf("How much money do you need to %s?: ", transaction_type);
-                Scanner transaction = new Scanner(System.in);
-                int amount = transaction.nextInt();
-                if (transaction_type.equals("deposit")) {
-                    if (amount > 0) {
-                        FileWriter update_balance = new FileWriter("balance.txt");
-                        update_balance.write(String.valueOf(current_balance + amount));
-                        current_balance += amount;
-                        update_balance.close();
-                    } else {
-                        System.out.println("Please enter a positive value");
-                        continue;
+            enter_amount.addActionListener((ActionListener) -> {
+                    if (input_amount.getText().isBlank())
+                    {
+                        current_balance_display.setText("Please enter a value");
+                        input_amount.setText("");
+                        enter_amount.setEnabled(false);
                     }
-                } else {
-                    if (amount < current_balance && amount > 0) {
-                        FileWriter update_balance = new FileWriter("balance.txt");
-                        int withdraw_balance = current_balance - amount;
-                        update_balance.write(String.valueOf(withdraw_balance));
-                        current_balance = withdraw_balance;
-                        update_balance.close();
-                    } else {
-                        System.out.println("Please enter a valid amount");
-                        continue;
+                    another_transaction.setVisible(true);
+                    if (type_of_transaction.getSelectedItem() == "Deposit"){
+                        float value = Float.parseFloat(input_amount.getText());
+                        try {
+                            if (value > 0) {
+                                FileWriter update_balance = new FileWriter("balance.txt");
+                                update_balance.write(String.valueOf(current_balance[0] + value));
+                                current_balance[0] += value;
+                                update_balance.close();
+                                current_balance_display.setText(String.format("Congratulations! Your new balance is: £%s", df2.format(current_balance[0])));
+                                input_amount.setText("");
+                                enter_amount.setEnabled(false);
+                            }else{
+                                current_balance_display.setText("Please enter a valid amount.");
+                            }
+                        }catch (IOException a) {
+                            System.out.println("An error occurred.");
+                            a.printStackTrace();
+                        }
                     }
-                }
-                String confirmation = String.format("£%d %s, your new balance is £%d", amount, past_tense_type, current_balance);
-                System.out.println(confirmation);
-                running = false;
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
+                    else if (type_of_transaction.getSelectedItem() == "Withdrawal"){
+                        float value = Float.parseFloat(input_amount.getText());
+                        try {
+                            if (value > 0 && (current_balance[0] - value) > 0) {
+                                FileWriter update_balance = new FileWriter("balance.txt");
+                                update_balance.write(String.valueOf(current_balance[0] - value));
+                                current_balance[0] -= value;
+                                update_balance.close();
+                                current_balance_display.setText(String.format("Congratulations! Your new balance is: £%s",df2.format(current_balance[0])));
+                                input_amount.setText("");
+                                enter_amount.setEnabled(false);
+                            }else {
+                                current_balance_display.setText("Please enter a valid amount.");
+                                input_amount.setText("");
+                                enter_amount.setEnabled(false);
+                            }
+                        }catch (IOException a) {
+                            System.out.println("An error occurred.");
+                            a.printStackTrace();
+                        }
+                    }
+                    else if (type_of_transaction.getSelectedItem() == ""){
+                        current_balance_display.setText("Please enter the type of transaction.");
+                        input_amount.setText("");
+                        enter_amount.setEnabled(false);
+                    }
+            });
+            another_transaction.addActionListener((ActionListener) ->
+                    enter_amount.setEnabled(true)
+            );
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         bank_app();
     }
 }
