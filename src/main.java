f7b0f89fa22f890eb57202c8cd96d84f42f0ea28
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-07-15 10:11:28 +0100

    Working on a GUI

--- src/main.java
+++ src/main.java
@@ -1,8 +1,72 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.io.*;
 import java.util.Scanner;
 
 class Banking{
-    public static void main(String[] args) {
+    private static void bank_app() {
+        try {
+            File check_balance = new File("balance.txt");
+            Scanner checker = new Scanner(check_balance);
+            int current_balance = checker.nextInt();
+
+            JLabel title = new JLabel("Welcome to the transaction app");
+            JLabel input_prompt = new JLabel("Input amount: ");
+            JTextField input_amount = new JTextField(20);
+            JComboBox<String> type_of_transaction = new JComboBox<>();
+            JButton enter_amount = new JButton("Enter");
+            JLabel current_balance_display = new JLabel(String.format("Current balance is: %d", current_balance));
+
+            /////////// GUI elements set up ///////////////////
+
+            type_of_transaction.addItem("Withdrawal");
+            type_of_transaction.addItem("Deposit");
+
+            Font font = new Font(Font.SERIF, Font.BOLD, 24);
+            Border raised = BorderFactory.createRaisedBevelBorder();
+
+            JFrame main_frame = new JFrame("Transaction app");
+            main_frame.setBounds(100, 100, 600, 400);
+            main_frame.setLayout(null);
+            main_frame.setResizable(false);
+            main_frame.setVisible(true);
+
+            JPanel title_panel = new JPanel();
+            title_panel.setBounds(0, 0, 600, 70);
+            main_frame.add(title_panel);
+            title.setFont(font);
+            title_panel.add(title);
+
+            JPanel main_panel = new JPanel(new GridBagLayout());
+            main_panel.setBounds(0, 67, 600, 300);
+            main_panel.setBorder(raised);
+            main_frame.add(main_panel);
+
+            GridBagConstraints constraints = new GridBagConstraints();
+            constraints.anchor = GridBagConstraints.WEST;
+            constraints.insets = new Insets(10, 10, 10, 10);
+            constraints.gridx = 0;
+            constraints.gridy = 0;
+
+            main_panel.add(current_balance_display, constraints);
+
+            constraints.gridy = 1;
+            main_panel.add(input_prompt, constraints);
+
+            constraints.gridx = 1;
+            main_panel.add(input_amount, constraints);
+
+            constraints.gridy = 2;
+            constraints.gridx = 0;
+            main_panel.add(type_of_transaction, constraints);
+
+            constraints.gridx = 1;
+            main_panel.add(enter_amount, constraints);
+        }catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
         boolean running = true;
         while (running) {
             try {
@@ -26,6 +90,10 @@ class Banking{
                         transaction_type = "withdraw";
                         past_tense_type = "withdrawn";
                         break;
+                    case "cancel":
+                        System.out.println("Thank you");
+                        running = false;
+                        return;
                     default:
                         System.out.println("Transaction cancelled.");
                         continue;
@@ -65,5 +133,8 @@ class Banking{
                 e.printStackTrace();
             }
         }
+    }
+    public static void main(String[] args){
+        bank_app();
     }
 }
